956eb4bd1795c26ba3b05edb284993038402d75d
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:27:37 +0300

    task_3

---
+++ SEM6/task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+bool IsPalindrome(string str)
+{
+    string normalized = new string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    return normalized.SequenceEqual(normalized.Reverse());
+}
+
+string input = "шалаш";
+bool isPalindrome = IsPalindrome(input);
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
