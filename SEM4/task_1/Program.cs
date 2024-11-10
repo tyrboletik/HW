12619384b369f99f1760cbe2e2b58a2e7356e829
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:11:45 +0300

    task_1

---
+++ SEM4/task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа
+// с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+while (true) 
+{
+    Console.Write("Введите число или 'q' для выхода: ");
+    string input = Console.ReadLine();
+    if (input == "q")
+    {
+        break;
+    }
+
+    int number;
+    int sum = 0;
+    if (int.TryParse(input, out number))
+
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+    else
+    {
+        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
+    }
+    if (sum % 2 == 0)
+    {
+        Console.WriteLine("[STOP]");
+        break;
+    }
+}
