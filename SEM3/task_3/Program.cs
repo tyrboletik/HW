ee83d73ceed9d55f0f2589ccdcb1e3fb054d060e
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:08:13 +0300

    task_3

---
+++ SEM3/task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿// Описание: Задайте массив из 10 целых чисел. Найдите среднее значение элементов массива.
+
+int[] array = { 11, 25, 34, 49, 53, 26, 87, 8, 95, 16 };
+int i = 0;
+double sum = 0;
+
+while (i < array.Length)
+{
+ sum = sum + array[i];
+ i++;
+}
+
+double average = sum / array.Length;
+{
+    Console.Write(average);
+}
