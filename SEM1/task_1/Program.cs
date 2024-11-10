cb502d1d05d4f793de738cb2c7e40b8a80226c7e
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 14:41:26 +0300

    task_1

---
+++ SEM1/task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите метод, который на вход принимает два целых числа и проверяет, делится ли первое число на второе.
+// Если делится, выводите "делится", иначе выводите "не делится".
+
+Console.Write("firstNumber: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("secondNumber: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+{
+    if (secondNumber == 0)
+    {
+        Console.WriteLine("На ноль делить нельзя");
+    }
+    else if (firstNumber % secondNumber == 0)
+    {
+        Console.WriteLine("делится");
+    }
+    else
+    {
+        Console.WriteLine("не делится");
+    }
+}
