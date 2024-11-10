3ca070e210bd8d4d025d2fb11e1ea6cd07399c2b
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 14:45:12 +0300

    task_2

---
+++ SEM1/task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿// Напишите метод, который принимает на вход три числа и возвращает среднее из этих чисел
+// (то есть не самое большое и не самое маленькое).
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+{
+    while ((a >= b && a <= c) || (a >= c && a <= b))
+    {
+        Console.Write(a);
+        return;
+        
+    }
+    if ((b >= a && b <= c) || (b >= c && b <= a))
+    {
+        Console.Write(b);
+        return; 
+    }
+    else
+    {
+        Console.Write(c);
+        return; 
+    }
+}
