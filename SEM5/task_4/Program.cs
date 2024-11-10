03c9aa2827f24bacb98fd1a50eb589eac6a38b7e
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:25:08 +0300

    task_4

---
+++ SEM5/task_4/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив. Замените все отрицательные числа на их
+// абсолютные значения.
+// Пример входных данных:
+// 1 -2 3
+// -4 5 -6
+// 7 -8 9
+// Пример результата:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+void ReplaceNegativeNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 0)
+            {
+                array[i, j] = array[i, j] * (-1);
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d =
+{
+{ 1, -2, 3 },
+{ -4, 5, -6 },
+};
+
+ReplaceNegativeNumbers(array2d);
+PrintArray(array2d);
