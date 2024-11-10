0672578c4caf8ca0ae1a31b4bcd9067d18f5efd6
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:17:47 +0300

    task_2

--- SEM3/task_2/Program.cs
+++ SEM3/task_2/Program.cs
@@ -5,7 +5,7 @@ int[] array = new int[10] { -3, 4, -5, 6, 8, -4, 7, 9, -1, 3 };
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] *= -1; 
+    array[i] *= -1;
 }
 Console.Write("[");
 for (int i = 0; i < array.Length; i++)
