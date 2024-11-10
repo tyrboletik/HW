ecfbf36c451b0e9f31468e631b6c4db6dceec5a8
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:02:27 +0300

    task_4

---
+++ SEM2/task_4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а 
+// на выходе показывает его цифры через запятую.
+
+int num;
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int numCopy = num;
+int count = 0;
+
+while (numCopy != 0)
+{
+    numCopy /= 10;
+    count++;
+}
+
+int[] arr = new int[count];
+int index = arr.Length - 1;
+while (num != 0)
+{
+    arr[index] = num % 10;
+    num /= 10;
+    index--;
+}
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]}, ");
+}
