b1fa5d8fd9c99c0339a8752c6cbfc7daf032f0d1
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:00:53 +0300

    task_3

---
+++ SEM2/task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 10 && num <= 99)
+{
+    int firstdDigit = num / 10;
+    int secondDigit = num % 10;
+
+    if (firstdDigit > secondDigit)
+    {
+        Console.WriteLine(firstdDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
