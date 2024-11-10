2ab16d51b5b19a3dee869f06abca7b543ac7d0d4
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:28:35 +0300

    task_4

---
+++ SEM6/task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте строку, состоящую из слов, разделенных пробелами. 
+//Сформировать строку, в которой слова расположены в обратном порядке. 
+//В полученной строке слова должны быть также разделены пробелами.
+static string NewMethod(string strNew)
+{
+    string[] strNewSplit = strNew.Split();
+
+    string res = "";
+
+    for (int i = strNewSplit.Length - 1; i >= 0; i--)
+    {
+        res += strNewSplit[i] + " ";
+    }
+
+    return res;
+}
+
+string strNew = "Hello my world";
+string res = NewMethod(strNew);
+Console.WriteLine(res);
