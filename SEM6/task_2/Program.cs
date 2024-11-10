cad26e9210dde579d492589e12ed4772221fa29e
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:26:54 +0300

    task_2

---
+++ SEM6/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+static string UpperToLower(string str)
+{
+    string strResult = string.Empty;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (char.IsUpper(str[i]))
+        {
+            strResult += char.ToLower(str[i]);
+        }
+        else
+        {
+            strResult += str[i];
+        }
+    }
+
+    return strResult;
+}
+
+string str = "aBcDlef!-";
+
+string strResult = UpperToLower(str);
+
+Console.WriteLine(strResult);
