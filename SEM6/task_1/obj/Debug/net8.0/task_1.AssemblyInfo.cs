0d5eb5ae2e2580de150718077910821319fbb3cc
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 18:46:29 +0300

    ok

--- SEM6/task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
+++ SEM6/task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("task_4")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab16d51b5b19a3dee869f06abca7b543ac7d0d4")]
-[assembly: System.Reflection.AssemblyProductAttribute("task_4")]
-[assembly: System.Reflection.AssemblyTitleAttribute("task_4")]
+[assembly: System.Reflection.AssemblyProductAttribute("task_1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
