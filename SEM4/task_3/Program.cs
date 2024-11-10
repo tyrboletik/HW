dcbe2046850ab55cc2a5f3c3be7d094c712c1057
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:14:11 +0300

    task_3

---
+++ SEM4/task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая перевернёт одномерный массив 
+
+{
+    int[] numbers = { 1, 2, 3, 4, 5, 6 };
+    int temp;
+
+    foreach (int number in numbers)
+    {
+        Console.Write($"{number} ");
+    }
+
+    for (int i = 0; i < numbers.Length / 2; i++)
+    {
+        temp = numbers[i];
+        numbers[i] = numbers[numbers.Length - 1 - i];
+        numbers[numbers.Length - 1 - i] = temp;
+    }
+
+    Console.Write(" => ");
+    foreach (int number in numbers)
+    {
+        Console.Write($"{number} ");
+    }
+}
