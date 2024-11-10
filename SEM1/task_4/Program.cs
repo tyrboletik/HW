313ce5fa944a70132cea948d1d95bd1b8875ac68
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 14:52:29 +0300

    task_4

---
+++ SEM1/task_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите метод, который на вход принимает число (number),
+// а на выходе выводит все нечетные числа от 1 до number (включительно),
+// после каждого числа должен быть знак пробела.
+
+Console.WriteLine("number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= num; i += 2)
+{
+    Console.Write(i + " ");
+}
