6048be333477c54e510dfe04b6fdf9df0cc725d9
Serega Bayern <pechkurovsg@mail.ru>
2024-11-10 15:09:35 +0300

    task_4

---
+++ SEM3/task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿﻿// Описание: Задайте массив целых чисел. Напишите программу, которая удаляет все отрицательные числа из массива и возвращает новый массив,
+// содержащий только неотрицательные числа.
+
+int[] numbers = { 10, -5, 16, -10, 0, 23, -71, 30, };
+int[] RemoveNegatives(int[] numbers)
+{
+    return numbers.Where(n => n >= 0).ToArray();
+}
+{
+    int[] result = RemoveNegatives(numbers);
+    Console.WriteLine("[" + string.Join(", ", result) + "]");
+}
